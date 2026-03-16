045f62e48d7f989dbcbecdad906d557ffb5448cc
James Finnon <jamesdfinnon@outlook.com>
2026-03-16 11:38:52 +0000

    added formatter method for time of message - might be needed later

--- Message.java
+++ Message.java
@@ -1,10 +1,28 @@
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class Message {
 	private String content;
 	private boolean read;
 	private Instant timeOfMessage;
 	
+	
+	/**
+	 * formats the time inputed to hours:minutes:seconds
+	 * taken from the emergency system assignment
+	 * 
+	 * @author jamesfinnon
+	 * @param time
+	 * @return
+	 */
+	public String formatTime(Instant time) {
+    	DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("HH:mm:ss")
+                        .withZone(ZoneId.systemDefault());
+    	
+    	return formatter.format(time);
+    }	
 	
 	public String getContent() {
 		return content;
