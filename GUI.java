3c2d9b838c4f8ca3f6b778419cbd11a145466c01
Faisal Yero <faisalyero21@gmail.com>
2026-03-17 15:52:37 +0000

    added 'new chat' button and used border layout for better organization

--- GUI.java
+++ GUI.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class GUI {
+public class GUI {  
     public static void main(String[] args) {
         // use the Swing 'invokeLater' method to create a new
         // Runnable object to run on the EDT
@@ -19,25 +19,58 @@ public class GUI {
     }
 
     public static void runProgram() {
+        //font
+        Font headerFont = new Font("Arial", Font.BOLD, 16);
+
+
+        String profileName = new String("Faisal S Yero");
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setSize(442, 874);
         window.setTitle("Messaging App");
         window.setLocationRelativeTo(null);
-        window.setLayout(null);
-        JLabel appName = new JLabel("Messages");
-        appName.setBounds(10,10,70,20);
-        window.add(appName);
-        JButton searchBut = new JButton("Search");
-        searchBut.setBounds(160, 10, 70, 20);
-        window.add(searchBut);
-        JButton profileBut = new JButton("Profile");
-        profileBut.setBounds(245, 10, 70, 20);
-        window.add(profileBut);
-        JButton contactsBut = new JButton("Contacts");
-        contactsBut.setBounds(320, 10, 70, 20);
-        window.add(contactsBut);
+        window.setLayout(new BorderLayout());
 
+        //hp
+        JPanel headerP = new JPanel();
+        headerP.setLayout(new BorderLayout());
+        headerP.setPreferredSize(new Dimension(442, 60));
+        
+        //lhp
+        JPanel headerLeft = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JLabel appName = new JLabel("Messages");
+        appName.setFont(headerFont);
+        headerLeft.add(appName);
+        headerP.add(headerLeft, BorderLayout.WEST);
+        
+        //rhp
+        JPanel headerRight = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JButton searchBut = new JButton("Search");
+        JButton profileBut = new JButton("Profile");
+        JButton contactsBut = new JButton("Contacts");
+        headerRight.add(searchBut);
+        headerRight.add(profileBut);
+        headerRight.add(contactsBut);
+        headerP.add(headerRight, BorderLayout.EAST);
+        
+        JLabel mainPName = new JLabel("Your name: " + profileName);
+        headerP.add(mainPName, BorderLayout.SOUTH);
+
+        //mp
+        JPanel mainP = new JPanel();
+        mainP.setLayout(new BorderLayout());
+
+        //fp
+        JPanel footerP = new JPanel();
+        footerP.setLayout(new BorderLayout());
+        footerP.setPreferredSize(new Dimension(442, 60));
+        
+        JButton newChatBut = new JButton("+ New Chat");
+        footerP.add(newChatBut, BorderLayout.CENTER);
+
+        window.add(headerP, BorderLayout.NORTH);
+        window.add(mainP, BorderLayout.CENTER);
+        window.add(footerP, BorderLayout.SOUTH);
 
         window.setVisible(true);
     }
