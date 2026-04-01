63f26a20e08b97d0dad67713ac711b01ee6b7bf6
James Finnon <jamesdfinnon@outlook.com>
2026-04-01 12:29:32 +0100

    added search for messages

--- Chat.java
+++ Chat.java
@@ -1,12 +1,30 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Set;
 
 public class Chat {
     private LinkedList<Message> messages;
+    
+    // set as can be unordered
     private Set<Contact> chatMembers;
 
 	public LinkedList<Message> getMessages() {
 		return messages;
+	}
+	
+	public ArrayList<Message> searchMessages(String searchWord, int searchBy) {
+		
+		Message message = new Message();
+		ArrayList<Message> matches = new ArrayList<Message>();
+		
+		for (int i = 0; i < messages.size(); i++) {
+			message = messages.get(i);
+				
+			if (messages.get(i).getContent().toLowerCase().contains(searchWord.toLowerCase())) {
+				matches.add(message);
+			}
+		}
+		return matches;
 	}
 
 	public void setMessages(LinkedList<Message> messages) {
