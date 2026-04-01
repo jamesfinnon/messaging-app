98aeb81a5011a535a174cd00887fc5d4fa17d863
James Finnon <jamesdfinnon@outlook.com>
2026-04-01 11:02:07 +0100

    added search contacts by name or username

--- User.java
+++ User.java
@@ -6,11 +6,40 @@ public class User extends Contact {
     public User() {
     	ArrayList<Contact> contacts = new ArrayList<Contact>();    	   	
     }
-
+    
 	public void addContact(Contact contact) {
 		contacts.add(contact);
 	}
     
+	public ArrayList<Contact> searchContacts(String searchWord, int searchBy) {
+		
+		Contact contact = new Contact();
+		ArrayList<Contact> matches = new ArrayList<Contact>();
+		
+		if (searchBy == 0) {
+			for (int i = 0; i < contacts.size(); i++) {
+				contact = contacts.get(i);
+				
+				if (contacts.get(i).getName().contains(searchWord)) {
+					matches.add(contact);
+				}
+			}
+		}
+		else if (searchBy == 1) {
+			for (int i = 0; i < contacts.size(); i++) {
+				contact = contacts.get(i);
+				
+				if (contacts.get(i).getUsername().contains(searchWord)) {
+					matches.add(contact);
+				}
+			}
+		}
+		return matches;
+		
+		
+		
+	}
+	
     // getters and setters
 	public ArrayList<Contact> getContacts() {
 		return contacts;
